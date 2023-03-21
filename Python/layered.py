9439d1e5528476c1dd606c4a5329c8250d84aab0
Jonas Hjulstad <jonasbhjulstad@gmail.com>
2023-03-21 20:48:44 +0100

    .

--- Python/layered.py
+++ Python/layered.py
@@ -9,10 +9,11 @@ import networkx as nx
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
+filepath = "/home/man/Documents/ER_Bernoulli/build/"
 
 class LayeredNetworkGraph(object):
 
-    def __init__(self, graphs, node_labels=None, layout=nx.spring_layout, ax=None):
+    def __init__(self, cluster_graphs,  graphs, node_labels=None, layout=nx.spring_layout, ax=None, y_offset=0):
         """Given an ordered list of graphs [g1, g2, ..., gn] that represent
         different layers in a multi-layer network, plot the network in
         3D with the different layers separated along the z-axis.
@@ -37,7 +38,8 @@ class LayeredNetworkGraph(object):
             The axis to plot to. If None is given, a new figure and a new axis are created.
 
         """
-
+        self.y_offset = y_offset
+        self.Gc = cluster_graphs
         # book-keeping
         self.graphs = graphs
         self.total_layers = len(graphs)
@@ -101,12 +103,25 @@ class LayeredNetworkGraph(object):
         for h in self.graphs[1:]:
             composition = nx.compose(composition, h)
 
-        pos = self.layout(self.graphs[0], *args, **kwargs)
-        # pos = self.layout(composition, *args, **kwargs)
+        N_pop_0 = self.Gc[0].number_of_nodes()
+        pos_0 = self.layout(self.Gc[0], *args, **kwargs)
+        pos_1 = self.layout(self.Gc[1], *args, **kwargs)
+        pos = dict()
+        for i in range(N_pop_0):
+            pos[i] = pos_0[i]
+        for i in range(N_pop_0, N_pop_0 + self.Gc[1].number_of_nodes()):
+            pos[i] = (pos_1[i - N_pop_0][0], pos_1[i - N_pop_0][1])
 
         self.node_positions = dict()
         for z, g in enumerate(self.graphs):
             self.node_positions.update({(node, z) : (*pos[node], z) for node in g.nodes()})
+
+        # pos = self.layout(self.graphs[0], k=100, *args, **kwargs)
+        # # pos = self.layout(composition, *args, **kwargs)
+
+        # self.node_positions = dict()
+        # for z, g in enumerate(self.graphs):
+        #     self.node_positions.update({(node, z) : (*pos[node], z) for node in g.nodes()})
         # self.node_positions = dict()
         # for z, g in enumerate(self.graphs):
         #     pos = self.layout(g, scale=2,*args, **kwargs)
@@ -115,13 +130,18 @@ class LayeredNetworkGraph(object):
 
     def draw_nodes(self, nodes, *args, **kwargs):
         x, y, z = zip(*[self.node_positions[node] for node in nodes])
+        #add y offset
+        y = [y[i] + self.y_offset for i in range(len(y))]
         self.ax.scatter(x, y, z, *args, **kwargs)
 
 
-    def draw_edges(self, edges, *args, **kwargs):
-        segments = [(self.node_positions[source], self.node_positions[target]) for source, target in edges]
-        line_collection = Line3DCollection(segments, *args, **kwargs)
-        self.ax.add_collection3d(line_collection)
+    def draw_edges(self, edges, alphas, *args, **kwargs):
+        for z, (G, a) in enumerate(zip(self.graphs, alphas)):
+            segment = [(self.node_positions[(source,z)], self.node_positions[(target,z)]) for source, target in G.edges()]
+            #add y offset
+            segment = [(self.node_positions[(source,z)], (self.node_positions[(target,z)][0], self.node_positions[(target,z)][1] + self.y_offset, self.node_positions[(target,z)][2])) for source, target in G.edges()]
+            coll = Line3DCollection(segment, alpha=a, *args, **kwargs)
+            self.ax.add_collection3d(coll)
 
 
     def get_extent(self, pad=0.1):
@@ -136,6 +156,8 @@ class LayeredNetworkGraph(object):
 
     def draw_plane(self, z, *args, **kwargs):
         (xmin, xmax), (ymin, ymax) = self.get_extent(pad=0.1)
+        ymin = ymin + self.y_offset
+        ymax = ymax + self.y_offset
         u = np.linspace(xmin, xmax, 10)
         v = np.linspace(ymin, ymax, 10)
         U, V = np.meshgrid(u ,v)
@@ -151,16 +173,16 @@ class LayeredNetworkGraph(object):
 
     def draw(self, color='white', alpha=1, plane_alpha=0.2, plane_color='gray', draw_node_labels=False, edge_alpha=0.2):
 
-        self.draw_edges(self.edges_within_layers,  color='k', alpha=edge_alpha, linestyle='-', zorder=2)
+        alphas = np.linspace(1, 0.1, self.total_layers)
+        self.draw_edges(self.edges_within_layers,  alphas, color='k', linestyle='-', zorder=2)
         # self.draw_edges(self.edges_between_layers, color='k', alpha=edge_alpha, linestyle='--', zorder=2)
-
         for z in range(self.total_layers):
-            self.draw_plane(z, alpha=plane_alpha,color=plane_color, zorder=1)
+            self.draw_plane(z, alpha=alphas[z]*plane_alpha,color=plane_color, zorder=1)
             #increase node edge thickness
             # self.draw_nodes([node for node in self.nodes if node[1]==z], s=300, zorder=3, marker="$\u25EF$", color='k', linewidth=1)
             # self.draw_nodes([node for node in self.nodes if node[1]==z], s=300, zorder=3, color=color, alpha=1)
-            self.draw_nodes(self.nodes, s=300, zorder=4, marker="$\u25EF$", color='k', linewidth=3)
-            self.draw_nodes(self.nodes, s=300, zorder=3, color=color, alpha=1)
+            self.draw_nodes(self.nodes, s=300, zorder=4, marker="$\u25EF$", color='k', linewidth=1, alpha=alphas[z])
+            self.draw_nodes(self.nodes, s=300, zorder=3, color=color, alpha=alphas[z])
             
 
         if self.node_labels and draw_node_labels:
@@ -172,32 +194,86 @@ class LayeredNetworkGraph(object):
 
 if __name__ == '__main__':
 
-    N_pop = 30
+    N_pop = 10
 
     #linear decrease of p_self, increase of p_other
-    p_other = np.logspace(-3,0, 4)
+    p_connect = np.linspace(0.1, 0.7, 5)
+    # p_connect = np.logspace(-1, 0, 5)
     # p_other = np.linspace(0.05, 0.6, 3)
     G = []
     #create two complete graphs with N_pop
 
     G0 = nx.complete_graph(int(N_pop))
+    #id offset N_pop
     G1 = nx.complete_graph(int(N_pop))
+    G1 = nx.relabel_nodes(G1, {k: k + N_pop for k in G1.nodes()})
+
+    G0_pos = nx.spring_layout(G0)
+    G1_pos = nx.spring_layout(G1)
+    #combine and add id offset
+    #concatenate G0_pos to G1_pos
+    #offset G1_pos keys by N_pop
+    #offset x by width of G0
+    G1_pos = {k: (v[0] + 5, v[1]) for k, v in G1_pos.items()}
 
 
-    #random connect the G0, G1
-    for i in range(len(p_other)):
-        #bipartite
-        G.append(nx.bipartite.random_graph(int(2*N_pop), int(2*N_pop), p_other[i]))
-        G[-1].add_edges_from(G0.edges())
-        G[-1].add_edges_from((N_pop + x, N_pop + y) for x,y in G1.edges_iter())
+    G_pos = {**G0_pos, **G1_pos}
 
-        
+    G_cons = [[nx.erdos_renyi_graph(N_pop*2, p) for i in range(5)] for p in p_connect] 
+    #compose with
+    G_complete = nx.compose(G0, G1)
 
-    node_labels = {nn : str(nn) for nn in range(len(G)*N_pop)}
+    Gs = [[nx.compose(G_complete, Gc) for Gc in Gcp] for Gcp in G_cons]
 
-    # initialise figure and plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    LayeredNetworkGraph(G, node_labels=node_labels, ax=ax, layout=nx.kamada_kawai_layout)
-    ax.set_axis_off()
-    plt.show()
+    #draw all Gs to svg
+    for i, Gp in enumerate(Gs):
+        for j, G in enumerate(Gp):
+            pos=nx.kamada_kawai_layout(G)
+            # nx.draw(G, with_labels=False, node_color='white', node_size=300, font_size=8, width=0.5, edge_color='black', pos=nx.spring_layout(G))
+            
+            #draw without border
+
+            code = nx.to_latex(G, G_pos)
+
+            #code to file
+            codepath = filepath + 'G_{i}_{j}.tex'.format(i=i, j=j)
+            with open(codepath, 'w') as f:
+                f.write(code)
+
+
+            # nx.draw_networkx_nodes(G, pos=G_pos, node_color='white', node_size=300, edgecolors='black')
+            # nx.draw_networkx_edges(G, pos=G_pos, width=0.5, edge_color='black')
+            # nx.draw_networkx_edges(G1, pos=G1_pos, width=0.5, edge_color='black')
+
+            # #remove all figure borders
+            # plt.gca().spines['right'].set_visible(False)
+            # plt.gca().spines['left'].set_visible(False)
+            # plt.gca().spines['bottom'].set_visible(False)
+            # plt.gca().spines['top'].set_visible(False)
+
+
+            # # plt.savefig(filepath + 'G_{i}_{j}.svg'.format(i=i, j=j))
+            # plt.savefig(filepath + 'G_{i}_{j}.pdf'.format(i=i, j=j))
+            
+            # plt.close()
+
+    # node_labels = {nn : str(nn) for nn in range(len(G)*N_pop*2)}
+
+    # yspace = 10
+
+    # # initialise figure and plot
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # Layer_0 = LayeredNetworkGraph([G0, G1], Gs[0], node_labels=node_labels, ax=ax, layout=nx.spring_layout, y_offset=0)
+    # y_extent = Layer_0.get_extent()[-1][-1]
+    # Layer_1 = LayeredNetworkGraph([G0, G1], Gs[1], node_labels=node_labels, ax=ax, layout=nx.spring_layout, y_offset=y_extent + yspace)
+    # ax.set_axis_off()
+    #create a 3d gray box around the graphs
+
+    #rotate view
+    # ax.view_init(azim=0, elev=90)
+    # plt.show()
+    #import bpy
+    # import svg in blender using bpy
+
+
