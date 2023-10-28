2e47ed8db5d0408bf19d52477018d28d833654a6
Jonas <jonasbhjulstad@gmail.com>
2023-10-28 21:14:21 +0200

    default_target

--- cmake/default_targets.cmake
+++ cmake/default_targets.cmake
@@ -1,7 +1,8 @@
 
 function(default_configure_target target_name)
-    target_compile_options(${target_name} PUBLIC -fsycl ${SYCL_CUSTOM_FLAGS})
-    target_link_options(${target_name} PUBLIC -fsycl -fsycl-targets=${${PROJECT_NAME}_SYCL_TARGETS})
+    target_compile_options(${target_name} PUBLIC -fsycl ${SYCL_CUSTOM_FLAGS} -Wno-deprecated_declarations)
+    target_link_options(${target_name} PUBLIC -fsycl -fsycl-targets=${${PROJECT_NAME}_SYCL_TARGETS} -Wno-deprecated-declarations)
+
     target_include_directories(${target_name} PUBLIC $<BUILD_INTERFACE:${${PROJECT_NAME}_INCLUDE_DIR}> $<INSTALL_INTERFACE:include>)
     if (${ENABLE_CLANG_TIDY})
     set_target_properties(${target_name} PROPERTIES CXX_CLANG_TIDY "${CLANG_TIDY_COMMAND}")
